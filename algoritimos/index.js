cea5e09cd155ca142446b63a2f75a6f409bfd16b
Edilson Nogueira <edilsonnsf2005@gmail.com>
2024-07-31 18:00:01 -0300

    brincando com js
    there's many things that i can do and that I have to do

---
+++ algoritimos/index.js
@@ -0,0 +1,20 @@
+/*let A = 19;
+let B = 20;
+let C = 40;
+let result = A + B;
+
+console.log(`A soma entre A e B é ${result}`);
+
+let menor = result < C;
+
+console.log(`Esta soma é menor que C? ${menor}`);*/ //EX-01
+
+/*let num = 6;
+let result = num % 2;
+
+if(num % 2 == 1) {
+    console.log('Ímpar');
+} else {
+    console.log('Par');
+};*/ //EX-02
+
