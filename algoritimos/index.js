8d915578751a0036f34041d60bdf1d7335a46c0c
Edilson Nogueira <edilsonnsf2005@gmail.com>
2024-08-01 21:47:47 -0300

    brincation in js

--- algoritimos/index.js
+++ algoritimos/index.js
@@ -17,4 +17,36 @@ if(num % 2 == 1) {
 } else {
     console.log('Par');
 };*/ //EX-02
+/*
+let A = 16;
+let B = 16;
+let result = 0;
 
+if (A == B) {
+    result = A + B;
+} else {
+    result = A * B;
+}
+
+let C = result;
+
+console.log('O resultado é', C);*/ //EX-03
+
+/*let num = 12;
+let ant = num - 1;
+let suc = num + 1;
+
+console.log(`O antecessor de ${num} é ${ant}`)
+console.log(`O sucessor de ${num} é ${suc}`)*/ //EX-04
+
+/*let minimo = 1.293;
+let salario = 3.000;
+let calc = parseInt(salario / minimo);
+
+console.log(calc);*/ // EX-05
+
+/*let num = 5;
+let percent = num * 5/100;
+let reajuste = num + percent;
+
+console.log(reajuste);*/ // EX-06
--- jogo-da-velha/script.js
+++ jogo-da-velha/script.js
@@ -2,13 +2,29 @@
 
 const quadrante = document.querySelectorAll('.quadrante');
 const restart = document.getElementById('restart');
-let jogador1 = "X";
+let jogadorAtual = "X";
 let qmaior = ['', '', '', '', '', '', '', '', ''];
 let jogoInicio = true;
 
 const condicaoVitoria = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 7]];
 
-const clickNoQuadrante = function click() {
+const clickNoQuadrante = (click) => {
     const quadrante = click.target;
     const quadranteIndex = quadrante.getAttribute('data-index');
+
+    if (qmaior[quadranteIndex] !== '' || !jogoInicio) {
+        return;
+    }
+
+    atualizacaoQuadrante(quadrante, quadranteIndex);
+    checkResult();
+}
+
+const atualizacaoQuadrante = (quadrante, index) => {
+    qmaior[index] = jogadorAtual;
+    quadrante.conteudoTexto = jogadorAtual;
+};
+
+const trocarJogador = () => {
+    jogadorAtual = jogadorAtual === 'X' ? 'O' : 'X';
 }
